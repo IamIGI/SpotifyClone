75a4cee1d2b680e2b79c7b753e7b0fd1f5fdeaba
i.kłusek <J6ZkqwVUn>
2024-11-21 23:22:34 +0100

    SA-02-Added SongCard component

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,10 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa ';
+
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) =>
+  isPlaying && activeSong?.title === song.attributes.name ? (
+    <FaPauseCircle className="text-gray-300" onClick={handlePause} />
+  ) : (
+    <FaPlayCircle className="text-gray-300" onClick={handlePause} />
+  );
 
 export default PlayPause;
