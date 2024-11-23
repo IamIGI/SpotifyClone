fc5295f5bf887e75062cbbb90781b1644ad27114
i.kłusek <J6ZkqwVUn>
2024-11-23 10:01:11 +0100

    SA-03-Update of music track, to use currently fetched data

--- src/components/MusicPlayer/Track.jsx
+++ src/components/MusicPlayer/Track.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 
-const Track = ({ isPlaying, isActive, activeSong }) => (
-  <div className="flex-1 flex items-center justify-start">
-    <div className={`${isPlaying && isActive ? 'animate-[spin_3s_linear_infinite]' : ''} hidden sm:block h-16 w-16 mr-4`}>
-      <img src={activeSong?.images?.coverart} alt="cover art" className="rounded-full" />
+const Track = ({ isPlaying, isActive, activeSong }) => {
+  const songData = activeSong.attributes;
+
+  return (
+    <div className="w-[30%] flex items-center justify-start">
+      <div
+        className={`${
+          isPlaying && isActive ? 'animate-[spin_3s_linear_infinite]' : ''
+        } hidden sm:block h-16 w-16 mr-4`}
+      >
+        <img
+          src={songData?.artwork?.url}
+          alt="cover art"
+          className="rounded-full"
+        />
+      </div>
+      <div className="w-[100%]">
+        <p className="truncate text-white font-bold text-lg">
+          {songData?.name ? songData.name : 'No active Song'}
+        </p>
+        <p className="truncate text-gray-300">
+          {songData?.composerName ? songData.composerName : 'No active Song'}
+        </p>
+      </div>
     </div>
-    <div className="w-[50%]">
-      <p className="truncate text-white font-bold text-lg">
-        {activeSong?.title ? activeSong?.title : 'No active Song'}
-      </p>
-      <p className="truncate text-gray-300">
-        {activeSong?.subtitle ? activeSong?.subtitle : 'No active Song'}
-      </p>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Track;
